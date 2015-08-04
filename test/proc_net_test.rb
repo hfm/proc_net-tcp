a0bde043ed9a6d0ce80f9fad1513c8f66291203a
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-08-04 11:52:41 +0900

    ProcNet::Tcp instance can read file like /proc/net/tcp (failure test)

--- test/proc_net_test.rb
+++ test/proc_net_test.rb
@@ -1,11 +1,19 @@
 require 'test_helper'
 
 class TestProcNetBase < Test::Unit::TestCase
+  def setup
+    @tcp = ProcNet::Tcp.new
+  end
   def test_hex_to_ip
     assert_equal '98.138.253.109', ProcNet::Tcp.hex2ip('628AFD6D')
   end
 
   def test_hex_to_port
     assert_equal "22", ProcNet::Tcp.hex2port('0016')
+  end
+
+  def test_read
+    table = @tcp.read('sample_tcp.txt') # => Array
+    assert_equal 4, table.size
   end
 end
